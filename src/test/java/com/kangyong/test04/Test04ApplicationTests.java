dd183762123a29eeea47e3879bae06b27dd77f93
tank863523 <976026355@qq.com>
2023-05-23 22:32:48 +0800

    first commit all commit

---
+++ src/test/java/com/kangyong/test04/Test04ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kangyong.test04;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Test04ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
